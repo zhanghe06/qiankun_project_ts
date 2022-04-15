d2f9651afe763aee4e79e1bb99d2472ac1892fe8
Zhang He <zhang_he06@163.com>
2022-04-15 22:31:15 +0800

    Fix Micro APP

--- app_react/.rescriptsrc.js
+++ app_react/.rescriptsrc.js
@@ -1,6 +1,4 @@
 const { name } = require('./package');
-// const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-
 
 module.exports = {
   webpack: (config) => {
@@ -8,33 +6,6 @@ module.exports = {
     config.output.libraryTarget = 'umd';
     // config.output.jsonpFunction = `webpackJsonp_${name}`;
     config.output.globalObject = 'window';
-    // config.module.rules = [{
-    //   test: /\.less$/,
-    //   use: [
-    //     {
-    //       loader: 'style-loader',
-    //     },
-    //     {
-    //       loader: 'css-loader', // translates CSS into CommonJS
-    //     },
-    //     {
-    //       loader: 'less-loader', // compiles Less to CSS
-    //       options: {
-    //         lessOptions: { // 如果使用less-loader@5，请移除 lessOptions 这一级直接配置选项。
-    //           modifyVars: {
-    //             'primary-color': '#1DA57A',
-    //             'link-color': '#1DA57A',
-    //             'border-radius-base': '2px',
-    //             '@ant-prefix': 'app-react',
-    //           },
-    //           javascriptEnabled: true,
-    //         },
-    //       },
-    //     }],
-    // }];
-    // config.plugins = [
-    //   new ReactRefreshWebpackPlugin(),
-    // ].filter(Boolean)
 
     return config;
   },
